1e4ab977f24a5575f763eb4952a37184023383d9
Yao Sun <mr.sun.yao@gmail.com>
2017-07-23 19:07:49 -0400

    updates to tests

--- array/l_array.h
+++ array/l_array.h
@@ -12,7 +12,6 @@ typedef struct l_arr {
   size_t s; // size of the array
   size_t alloc; // allocated size of the array
   arr_type *b; // beginning of the logical array
-  arr_type *e;
 } arr;
 
 // allocates a new array structure onto the heap
@@ -20,19 +19,41 @@ arr * l_arr_init(int init_s, int blk_s) {
   assert(init_s >= 0);
   assert(blk_s > 0); // block size cant be 0!
 
-  arr *arr_m_ptr = (arr *) malloc(3 * sizeof(size_t) + 2 * sizeof(arr_type));
+  arr *arr_m_ptr = (arr *) malloc(3 * sizeof(size_t) + 1 * sizeof(arr_type));
   arr_m_ptr->blk_s = blk_s;
   arr_m_ptr->s = 0;
   arr_m_ptr->alloc = blk_s * ((init_s / blk_s) + 1);
   arr_m_ptr->b = (arr_type *) malloc(arr_m_ptr->alloc * sizeof(arr_type));
-  arr_m_ptr->e = arr_m_ptr->b;
 
   return arr_m_ptr;
 }
 
-arr * push(arr *arr_ptr, arr_type t) {
-  arr_ptr->e = arr_ptr->e + 1;
-  *arr_ptr->e = t;
+// resizes based on how many elements are being added
+static void l_arr_resize(arr *arr_ptr, int ele) {
+  if (arr_ptr->s + ele > arr_ptr->alloc) {
+    arr_ptr->alloc = ((arr_ptr->alloc + ele) / arr_ptr->blk_s + 1) * arr_ptr->blk_s;
+    arr_ptr->b = realloc(
+      arr_ptr->b,
+      arr_ptr->alloc
+    );
+  }
+}
+
+arr * l_arr_push(arr *arr_ptr, arr_type t) {
+  l_arr_resize(arr_ptr, 1);
+  arr_ptr->s = arr_ptr->s + 1;
+  *(arr_ptr->b + arr_ptr->s - 1) = t;
+  return arr_ptr;
+}
+
+arr_type l_arr_get(arr *arr_ptr, int i) {
+  assert(i < arr_ptr->s);
+  return *(arr_ptr->b + i);
+}
+
+void l_arr_free(arr *arr_ptr) {
+  free(arr_ptr->b);
+  free(arr_ptr);
 }
 
 #endif
--- array/l_array_test.c
+++ array/l_array_test.c
@@ -4,14 +4,34 @@
 MU_TEST(test_array_init) {
   // do nothing for now
   arr *arr_ptr = l_arr_init(1, 16);
-  printf("arr_ptr\nblk_size=%d\nsize=%d\nalloc=%d",
+  printf("test_array_init:\n");
+  printf("arr_ptr\nblk_size=%d\nsize=%d\nalloc=%d\n",
     arr_ptr->blk_s,
     arr_ptr->s,
     arr_ptr->alloc);
+
+  l_arr_free(arr_ptr);
+}
+
+MU_TEST(test_array_push) {
+  arr *arr_ptr = l_arr_init(1, 1);
+
+  printf("test_array_push:\n");
+  printf("arr_ptr\nblk_size=%d\nsize=%d\nalloc=%d\n",
+    arr_ptr->blk_s,
+    arr_ptr->s,
+    arr_ptr->alloc);
+
+  l_arr_push(arr_ptr, 1337);
+  int resp = l_arr_get(arr_ptr, 0);
+  printf("l_arr_get(0)=%d\n", resp);
+  
+  l_arr_free(arr_ptr);
 }
 
 MU_TEST_SUITE(test_suite) {
   MU_RUN_TEST(test_array_init);
+  MU_RUN_TEST(test_array_push);
 }
 
 int main(int argc, char *argv[]) {
